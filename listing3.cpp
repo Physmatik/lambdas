4d35e08edf8a825764b94bb73f51dd72077ca73f
Mykhailo <mikhail.nikolajchuk@gmail.com>
2017-10-30 08:04:55 +0200

    added listing3

---
+++ listing3.cpp
@@ -0,0 +1,25 @@
+#include <algorithm>
+#include <cstdlib>
+#include <iostream>
+#include <vector>
+ 
+using namespace std;
+ 
+int main()
+{
+	vector<int> srcVec;
+	for (int val = 0; val < 10; val++)
+	{
+		srcVec.push_back(val);
+	}
+ 
+	int result =
+		count_if(srcVec.begin(), srcVec.end(), [] (int _n)
+		{
+			return (_n % 2) == 0;
+		});
+ 
+	cout << result << endl;
+ 
+	return EXIT_SUCCESS;
+}
